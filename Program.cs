671864754ed66c8483c8d59f6111178a2b158cf3
Inferior Lemon <strusinskipawel@wp.pl>
2025-07-07 20:54:33 +0200

    Aktualizacja kodu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Using system;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Prosty kalkulator!");
+        Console.Write($"10 + 5 = ", { Add(10,5) });
+    }
+    static int Add(x, y)
+    {
+        return x + y
+    }
+}
