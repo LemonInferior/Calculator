cb725b1582963d236a94824af8d5187fbe0d4028
Inferior Lemon <strusinskipawel@wp.pl>
2025-07-07 21:00:44 +0200

    2 zmiana, fukcja odejmowania

--- Program.cs
+++ Program.cs
@@ -5,9 +5,14 @@ public class Program
     {
         Console.WriteLine("Prosty kalkulator!");
         Console.Write($"10 + 5 = ", { Add(10,5) });
+        Console.Write($"10 - 5 = ", { Sub(10, 5) });
     }
     static int Add(x, y)
     {
         return x + y
+    }
+    static int Sub(x, y)
+    {
+        return x - y
     }
 }
