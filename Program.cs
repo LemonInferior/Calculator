168f1eb3d2fa3faaab40b8739232d3d72b191972
Inferior Lemon <strusinskipawel@wp.pl>
2025-07-08 17:55:27 +0200

    Added multiplication & sproj file

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ public class Program
         Console.WriteLine("Prosty kalkulator!");
         Console.Write($"10 + 5 = ", { Add(10,5) });
         Console.Write($"10 - 5 = ", { Sub(10, 5) });
+        Console.Write($"10 * 5 = ", { Multiply(10, 5) });
     }
     static int Add(x, y)
     {
@@ -14,5 +15,9 @@ public class Program
     static int Sub(x, y)
     {
         return x - y
+    }
+    static int Multiply(x, y)
+    {
+        return x * y;
     }
 }
